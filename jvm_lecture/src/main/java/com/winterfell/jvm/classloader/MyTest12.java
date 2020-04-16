ee464232b08914270272a69a6855cfa03f8606a8
winterfell <mimotronik@gmail.com>
2020-04-17 01:08:41 +0800

    类加载器

---
+++ jvm_lecture/src/main/java/com/winterfell/jvm/classloader/MyTest12.java
@@ -0,0 +1,29 @@
+package com.winterfell.jvm.classloader;
+
+/**
+ * 调用ClassLoader类的loadClass方法加载一个类，并不是对类的主动使用，不会导致类的初始化
+ *
+ * @author winterfell
+ */
+public class MyTest12 {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+
+        ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+        Class<?> clazz = classLoader.loadClass("com.winterfell.jvm.classloader.CL");
+
+        System.out.println(clazz);
+
+        System.out.println("---------------");
+
+        Class.forName("com.winterfell.jvm.classloader.CL");
+    }
+
+}
+
+class CL {
+    static {
+        System.out.println("Class CL");
+    }
+}
---
+++ jvm_lecture/src/main/java/com/winterfell/jvm/classloader/MyTest16.java
@@ -0,0 +1,14 @@
+package com.winterfell.jvm.classloader;
+
+/**
+ * @author winterfell
+ */
+public class MyTest16 extends ClassLoader {
+    private String classLoaderName;
+    private final String fileExtension = ".class";
+
+    public MyTest16(String classLoaderName) {
+        super();
+
+    }
+}
